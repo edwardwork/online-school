eb196c77d96734140b2e709ad21b5808e16a879a
Edward <edwardworkru@gmail.com>
2020-11-19 14:53:42 +0200

    fix: Поправил название подключаемого файла

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <link rel="stylesheet" href="{{ mix('css/style.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
