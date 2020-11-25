0068a69cfc10c090e7ab02069cf364c15f0449fe
Edward <edwardworkru@gmail.com>
2020-11-25 23:25:30 +0200

    fix: поправил роут

--- resources/views/layouts/topics/index.blade.php
+++ resources/views/layouts/topics/index.blade.php
@@ -11,7 +11,7 @@
 					<div class="card text-center">
 						<div class="card-body">
 							<h5 class="card-title rem-3">{{ $topic->name }}</h5>
-							<a href="{{ route('topic.show', ['topic' => $topic->id]) }}" class="btn btn-primary rem-2">Список уроков</a>
+							<a href="{{ route('topics.show', ['topic' => $topic->id]) }}" class="btn btn-primary rem-2">Список уроков</a>
 						</div>
 					</div>
 				</div>
