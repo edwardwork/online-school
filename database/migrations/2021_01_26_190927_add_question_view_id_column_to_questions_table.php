4a1237e0f55be35d03a0129a92a67f9916368053
Edward <edwardworkru@gmail.com>
2021-01-26 21:14:36 +0200

    feat: добавил миграции + создал модель видов вопросов

---
+++ database/migrations/2021_01_26_190927_add_question_view_id_column_to_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddQuestionViewIdColumnToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->integer('question_view_id')
+                ->after('lesson_id')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
